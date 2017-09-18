034f601e6a2842108d4211bb2259051fa8a6a60f
pawel <pawel.dabrowski@unifiedfactory.com>
2017-09-18 17:34:55 +0200

    init

---
+++ app.js
@@ -0,0 +1,27 @@
+var list = document.getElementById('list');
+
+document.onload = loop();
+
+function loop() {
+    for (var i = 0; i < 100; i++) {
+        createElement(i)
+    }
+}
+
+function createElement(index) {
+    if (!index) return;
+
+    var createDiv = document.createElement("div");
+    var listItem = "element listy nr: " + index + "<span style=\"float: right\">usun</span>";
+
+    list.appendChild(createDiv).innerHTML = listItem;
+}
+
+function clickItem(e) {
+    if(!e) return;
+
+    if(e.target.textContent === "usun") {
+        e.target.parentNode.remove()
+    }
+
+}
